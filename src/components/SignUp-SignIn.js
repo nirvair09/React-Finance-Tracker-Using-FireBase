1f3befc351b8624bde9171118401da0a2f67ee4b
nirvair09 <chandradeep262003@gmail.com>
2025-10-25 21:51:03 +0530

    Header added

--- src/components/SignUp-SignIn.js
+++ src/components/SignUp-SignIn.js
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { auth, provider, db } from "../firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import Header from "./Header";
+import { toast } from "react-toastify";
+
 
 const SignUpSignIn = () => {
+  const [name,setName]=useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [flag,setFlag]=useState(false);
+  
+  const navigate = useNavigate();
   return (
     <div>SignUp-SignIn</div>
   )
