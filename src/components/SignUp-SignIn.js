bf6ab499cbf465e8f86c7dd0b9ef1a01939e2f30
nirvair09 <chandradeep262003@gmail.com>
2025-10-25 23:40:27 +0530

    signUp-signIn logic integrared

--- src/components/SignUp-SignIn.js
+++ src/components/SignUp-SignIn.js
@@ -20,9 +20,229 @@ const SignUpSignIn = () => {
   const [flag,setFlag]=useState(false);
   
   const navigate = useNavigate();
+
+  const creatUserDocument = async(user)=>{
+    setLoading(true);
+    if(!user) return;
+
+    const userRef = doc(db,"users",user.uid);
+    const userData = await getDoc(userRef);
+
+    if(!userData.exists()){
+      const {displayName,email,photoURL}=user;
+
+      const createdAt=new Date();
+
+      
+      try {
+        await setDoc(userRef, {
+          name: displayName ? displayName : name,
+          email,
+          photoURL: photoURL ? photoURL : "",
+          createdAt,
+        });
+        toast.success("Account Created!");
+        setLoading(false);
+      } catch (error) {
+        toast.error(error.message);
+        console.error("Error creating user document: ", error);
+        setLoading(false);
+      }
+    }
+  };
+
+  const signUpWithEmail = async(e)=>{
+    setLoading(true);
+    e.preventDefault();
+    try {
+      const result = await createUserWithEmailAndPassword(
+        auth,email,password
+      );
+
+      const user=result.user;
+      await creatUserDocument(user);
+      toast.success("Signed Up Successfully");
+      setLoading(false);
+      navigate("/dashboard");
+    } catch (error) {
+      toast.error(error.message);
+      console.error(
+        "Error signing up with email and password: ",
+        error.message
+      );
+      setLoading(false);
+    }
+  }
+
+  const signInWithEmail = async(e)=>{
+    setLoading(true);
+    e.preventDefault();
+    try {
+      const result = await signInWithEmailAndPassword(
+        auth,email,password);
+        const user=result.user;
+        toast.success("Signed In Successfully");
+        setLoading(false);
+    } catch (error) {
+      toast.error(error.message);
+      console.error(
+        "Error signing in with email and password: ",
+        error.message
+      );
+      setLoading(false);
+    }
+  }
+
+  const signInWithGoogle=async()=>{
+    setLoading(true);
+    try {
+      const resutl = await signInWithPopup(auth,provider);
+      const user=resutl.user;
+      await creatUserDocument(user);
+      toast.success("Signed In Successfully");
+      setLoading(false);
+      navigate("/dashboard");
+    } catch (error) {
+      setLoading(false);
+      toast.error(error.message);
+      console.error("Error signing in with Google: ", error.message);
+    }
+  }
+
   return (
-    <div>SignUp-SignIn</div>
-  )
+    <>
+      <Header />
+      <div className="wrapper">
+        {flag ? (
+          <div className="signup-signin-container">
+            <h2 style={{ textAlign: "center" }}>
+              Log In on <span className="blue-text">Financely.</span>
+            </h2>
+            <form onSubmit={signUpWithEmail}>
+              <div className="input-wrapper">
+                <p>Email</p>
+                <input
+                  type="email"
+                  placeholder="JohnDoe@gmail.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+
+              <div className="input-wrapper">
+                <p>Password</p>
+                <input
+                  type="password"
+                  placeholder="Example123"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+
+              <button
+                disabled={loading}
+                className="btn"
+                onClick={signInWithEmail}
+              >
+                {loading ? "Loading..." : " Log In with Email and Password"}
+              </button>
+            </form>
+            <p style={{ textAlign: "center", margin: 0 }}>or</p>
+            <button
+              disabled={loading}
+              className="btn btn-blue"
+              onClick={signInWithGoogle}
+            >
+              {loading ? "Loading..." : " Log In with Google"}
+            </button>
+            <p
+              onClick={() => setFlag(!flag)}
+              style={{
+                textAlign: "center",
+                marginBottom: 0,
+                marginTop: "0.5rem",
+                cursor: "pointer",
+              }}
+            >
+              Or Don't Have An Account? Click Here.
+            </p>
+          </div>
+        ) : (
+          <div className="signup-signin-container">
+            <h2 style={{ textAlign: "center" }}>
+              Sign Up on <span className="blue-text">Financely.</span>
+            </h2>
+            <form onSubmit={signUpWithEmail}>
+              <div className="input-wrapper">
+                <p>Full Name</p>
+                <input
+                  type="text"
+                  placeholder="John Doe"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className="input-wrapper">
+                <p>Email</p>
+                <input
+                  type="email"
+                  placeholder="JohnDoe@gmail.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+
+              <div className="input-wrapper">
+                <p>Password</p>
+                <input
+                  type="password"
+                  placeholder="Example123"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+
+              <div className="input-wrapper">
+                <p>Confirm Password</p>
+                <input
+                  type="password"
+                  placeholder="Example123"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+              </div>
+
+              <button type="submit" className="btn">
+                {loading ? "Loading..." : "Sign Up with Email and Password"}
+              </button>
+            </form>
+            <p style={{ textAlign: "center", margin: 0 }}>or</p>
+            <button
+              disabled={loading}
+              className="btn btn-blue"
+              onClick={signInWithGoogle}
+            >
+              {loading ? "Loading..." : "Sign Up with Google"}
+            </button>
+            <p
+              onClick={() => setFlag(!flag)}
+              style={{
+                textAlign: "center",
+                marginBottom: 0,
+                marginTop: "0.5rem",
+                cursor: "pointer",
+              }}
+            >
+              Or Have An Account Already? Click Here
+            </p>
+            {/* <button onClick={signInWithEmail}>
+            Sign In with Email and Password
+          </button> */}
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default SignUpSignIn
