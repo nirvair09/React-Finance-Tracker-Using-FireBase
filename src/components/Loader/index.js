330cede4eca8d96df012606b2e49d14d1142f140
nirvair09 <chandradeep262003@gmail.com>
2025-10-25 21:54:58 +0530

    loader added

---
+++ src/components/Loader/index.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+function Loader() {
+    return (
+        <div className="wrapper">
+            <div className="lds-ripple">
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Loader;
